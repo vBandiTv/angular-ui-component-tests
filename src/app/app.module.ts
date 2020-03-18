3f8ce715b42f999823c926ff2515867fdbe5755f
vBandiTv <fimabndt@gmail.com>
2020-03-18 11:58:18 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CollapseComponent } from './collapse/collapse.component';
+import { ChipsComponent } from './chips/chips.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CollapseComponent ],
+  declarations: [ AppComponent, HelloComponent, CollapseComponent, ChipsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
