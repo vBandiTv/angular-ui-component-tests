ac3e579f7b030efd9269bb16ced6dc6207528f60
vBandiTv <fimabndt@gmail.com>
2020-03-18 10:18:56 +0200

    4 files modified

--- src/app/collapse/collapse.component.ts
+++ src/app/collapse/collapse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-collapse',
@@ -11,5 +11,9 @@ export class CollapseComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  show: boolean;
+
+
 
 }
