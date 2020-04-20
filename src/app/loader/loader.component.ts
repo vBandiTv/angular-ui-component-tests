0d126da509fe4e6e212320c5e009ea1033760884
vBandiTv <fimabndt@gmail.com>
2020-04-20 13:31:45 +0300

    1 file modified, 1 added, 1 deleted

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -4,7 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css']
+  styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent {
 
